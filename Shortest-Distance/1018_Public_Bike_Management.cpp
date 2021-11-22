a83c45c8498f5099faf80edf9e3db36f30d6dfcc
Jianyang Zhang <82556150+LT-IssacF@users.noreply.github.com>
2021-11-22 13:39:33 +0800

    Update 1018_Public_Bike_Management.cpp

--- Shortest-Distance/1018_Public_Bike_Management.cpp
+++ Shortest-Distance/1018_Public_Bike_Management.cpp
@@ -6,16 +6,16 @@ using namespace std;
 struct node {
     int v, w, dis;
 } temp;
-vector<vector<node>> G;
+vector<vector<node>> G; // 邻接表
 vector<int> d, path, tempPath;
 vector<bool> visit;
 vector<vector<int>> pre; // 记录前驱
 int minRemain = INF, minNeed = INF;
-
+// 全局变量最少剩余车辆，最少初始携带车辆
 void Dijkstra( int N, int start ) {
     fill( d.begin( ), d.end( ), INF );
     fill( visit.begin( ), visit.end( ), false );
-    d[start] = 0;
+    d[start] = 0; // 初始城市处理
     for( int i = 0; i < N; i++ ) {
         int u = -1, MIN = INF;
         for( int j = 0; j < N; j++ ) {
@@ -43,8 +43,8 @@ void Dijkstra( int N, int start ) {
     }
 }
 
-void DFS( int v ) {
-    if( v == 0 ) { // 递归到边界即PBMC了
+void DFS( int v ) { // 从S倒着深度遍历回PBMC
+    if( v == 0 ) { // 递归到边界即PBMC就把这一路的结点计算
         tempPath.push_back( v );
         int remain = 0, need = 0, size = tempPath.size( );
         for( int i = size - 1; i >= 0; i-- ) {
@@ -70,13 +70,13 @@ void DFS( int v ) {
             minRemain = remain;
             path = tempPath;
         }
-        tempPath.pop_back( ); // 路径已在path中保存
-        return; // 这两个pop是为了回溯到分叉结点
+        tempPath.pop_back( ); // 真正保存结果的路径已在path中保存
+        return; // 这个和下面的俩个pop是为了回溯到分叉结点
     }
-    tempPath.push_back( v );
+    tempPath.push_back( v ); // 加入路径
     int size = pre[v].size( );
     for( int i = 0; i < size; i++ )
-        DFS( pre[v][i] );
+        DFS( pre[v][i] ); // 深度遍历v的前驱
     tempPath.pop_back( );
 }
 
@@ -92,7 +92,7 @@ int main( ) {
     for( int i = 1; i <= N; i++ ) {
         cin >> tempWeight[i];
         tempWeight[i] -= ( C / 2 );
-    }
+    } // 这里对点权车辆数量做了处理，取2/C为零点，方便计算
     for( int i = 0, c1 = 0, c2 = 0, tempDis = 0; i < M; i++ ) {
         cin >> c1 >> c2 >> tempDis;
         temp.dis = tempDis;

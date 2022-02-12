91149ac49c0b3b755da01d750b5f795a12d0f7ad
Jianyang Zhang <82556150+LT-IssacF@users.noreply.github.com>
2022-02-12 17:49:47 +0800

    Update 1020_Tree_Traversals.cpp

--- Tree/1020_Tree_Traversals.cpp
+++ Tree/1020_Tree_Traversals.cpp
@@ -3,12 +3,13 @@
 #include <vector>
 using namespace std;
 typedef struct BiNode {
-    int Data;
-    struct BiNode *Left, *Right;
+    int data;
+    BiNode *lchild, *rchild;
 } BiNode, *BiTree;
 int pre[32], post[32], in[32]; // 分别保存前序、中序、后序序列
-// 由前序和中序序列建树
-// BiTree Build_Tree( int preL, int preR, int inL, int inR ) {
+vector<int> ans;
+// // 由前序和中序序列建树
+// BiTree Build_Tree( const int &preL, const int &preR, const int &inL, const int &inR ) {
 //     if( preL > preR )
 //         return NULL;
 //     int i, numLeft;
@@ -16,58 +17,56 @@ int pre[32], post[32], in[32]; // 分别保存前序、中序、后序序列
 //         if( in[i] == pre[preL] )
 //             break;
 //     numLeft = i - inL; // 左子树的个数
-//     BiTree T = ( BiTree ) malloc( sizeof( struct BiNode ) );
-//     T->Data = pre[preL];
-//     T->Left = Build_Tree( preL + 1, preL + numLeft, inL, i - 1 );
-//     T->Right = Build_Tree( preL + numLeft + 1, preR, i + 1, inR );
+//     BiTree T = ( BiTree ) malloc( sizeof( BiNode ) );
+//     T->data = pre[preL];
+//     T->lchild = Build_Tree( preL + 1, preL + numLeft, inL, i - 1 );
+//     T->rchild = Build_Tree( preL + numLeft + 1, preR, i + 1, inR );
 //     return T;
 // }
-// 由中序和后序序列建树
-BiTree Build_Tree( int postL, int postR, int inL, int inR ) {
+// // 由后序和中序序列建树
+BiTree Insert( const int &postL, const int &postR, const int &inL, const int &inR ) {
     if( postL > postR )
         return NULL;
     int i, numLeft;
     for( i = inL; i <= inR; i++ )
         if( in[i] == post[postR] )
             break;
-    numLeft = i - inL; // 左子树的个数
-    BiTree T = ( BiTree ) malloc( sizeof( struct BiNode ) );
-    T->Data = post[postR];
-    T->Left = Build_Tree( postL, postL + numLeft - 1, inL, i - 1 );
-    T->Right = Build_Tree( postL + numLeft, postR - 1, i + 1, inR );
+    numLeft = i - inL;
+    BiTree T = ( BiTree ) malloc( sizeof( BiNode ) );
+    T->data = post[postR];
+    T->lchild = Insert( postL, postL + numLeft - 1 , inL, i - 1 );
+    T->rchild = Insert( postL + numLeft, postR - 1, i + 1, inR );
     return T;
 }
 
-void BFS( BiTree T ) {
+void BFS( const BiTree &T ) {
     queue<BiTree> q;
     q.push( T );
-    BiTree temp;
-    vector<int> v;
+    BiTree front;
     while( !q.empty( ) ) {
-        temp = q.front( );
+        front = q.front( );
+        ans.push_back( front->data );
         q.pop( );
-        v.push_back( temp->Data );
-        if( temp->Left )
-            q.push( temp->Left );
-        if( temp->Right )
-            q.push( temp->Right );
+        if( front->lchild != NULL )
+            q.push( front->lchild );
+        if( front->rchild != NULL )
+            q.push( front->rchild );
     }
-    int length = v.size( );
-    if( length )
-        cout << v[0];
-    for( int i = 1; i < length; i++ )
-        cout << " " << v[i];
 }
 
 int main( ) {
-    BiTree T;
     int N;
     cin >> N;
     for( int i = 0; i < N; i++ )
-        cin >> post[i];
+        scanf( "%d", &post[i] );
     for( int i = 0; i < N; i++ )
-        cin >> in[i];
-    T = Build_Tree( 0, N - 1, 0, N - 1 );
+        scanf( "%d", &in[i] );
+    BiTree T = Insert( 0, N - 1, 0, N - 1 );
     BFS( T );
+    for( int i = 0; i < ans.size( ); i++ ) {
+        if( i > 0 )
+            printf( " " );
+        printf( "%d", ans[i] );
+    }
     return 0;
 }

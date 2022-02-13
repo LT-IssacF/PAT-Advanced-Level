a8a14dcc9564bd0a0968e51cfb9ab7f2e89a9ae5
Jianyang Zhang <82556150+LT-IssacF@users.noreply.github.com>
2022-02-13 23:21:19 +0800

    Update 1028_List_Sorting.cpp

--- Sort/1028_List_Sorting.cpp
+++ Sort/1028_List_Sorting.cpp
@@ -5,38 +5,39 @@
 #include <algorithm>
 using namespace std;
 struct Record {
-    char ID[20], name[20];
+    char id[7], name[9];
     int grade;
 };
-vector<Record> stu;
-bool cmp_ID( const Record &a, const Record &b ) { // 按ID升序排序
-    return strcmp( a.ID, b.ID ) < 0;
+bool cmp_id( const Record &a, const Record &b ) { // 按id升序排序
+    return strcmp( a.id, b.id ) < 0;
 }
 bool cmp_name( const Record &a, const Record &b ) {
-    if( strcmp( a.name, b.name ) != 0 ) // 名字不同就按名字升序排序
+    if( strcmp( a.name, b.name ) != 0 ) { // 名字不同就按名字升序排序
         return strcmp( a.name, b.name ) < 0;
-    else // 名字相同就按ID升序排序
-        return strcmp( a.ID, b.ID ) < 0;
+    } else { // 名字相同就按id升序排序
+        return strcmp( a.id, b.id ) < 0;
+    }
 }
 bool cmp_grade( const Record &a, const Record &b ) {
-    if( a.grade != b.grade ) // 分数不同按分数升序排序
+    if( a.grade != b.grade ) { // 分数不同按分数升序排序
         return a.grade < b.grade;
-    else // 同样的
-        return strcmp( a.ID, b.ID ) < 0;
+    } else { // 同样的
+        return strcmp( a.id, b.id ) < 0;
+    }
 }
 // 送分题
 int main( ) {
     int N, C;
     cin >> N >> C;
-    stu.resize( N );
+    vector<Record> stu( N );
     for( int i = 0; i < N; i++ )
-        scanf( "%s %s %d", &stu[i].ID, &stu[i].name, &stu[i].grade );
+        scanf( "%s %s %d", &stu[i].id, &stu[i].name, &stu[i].grade );
     switch( C ) {
-        case 1: sort( stu.begin( ), stu.end( ), cmp_ID ); break;
+        case 1: sort( stu.begin( ), stu.end( ), cmp_id ); break;
         case 2: sort( stu.begin( ), stu.end( ), cmp_name ); break;
         case 3: sort( stu.begin( ), stu.end( ), cmp_grade );
     }
     for( int i = 0; i < N; i++ )
-        printf( "%s %s %d\n", stu[i].ID, stu[i].name, stu[i].grade );
+        printf( "%s %s %d\n", stu[i].id, stu[i].name, stu[i].grade );
     return 0;
 }

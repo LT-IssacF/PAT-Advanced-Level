a5a7ec4f8f7bda239958a40fae3c572ae59f38f2
Jianyang Zhang <82556150+LT-IssacF@users.noreply.github.com>
2022-02-16 15:05:47 +0800

    Update 1003_Emergency.cpp

--- Shortest-Distance/1003_Emergency.cpp
+++ Shortest-Distance/1003_Emergency.cpp
@@ -1,25 +1,27 @@
 #include <iostream>
+#include <cstdio>
 #include <vector>
-#include <algorithm>
 #define INF 0x3fffffff
 using namespace std;
 struct node {
-    int v, w, dis;
-}; // v为城市号，w为救援队数量点权，dis为距离边权
+    int v, dis;
+    node( const int & v2, const int &dis2 ) : v( v2 ), dis( dis2 ) { }
+}; // G[c1][i].v表示c1到其连通的第i个城市(i从0开始)的城市号
+// G[c1][i].dis表示c1到其连通的第i个城市的距离，G[c1][i].w表示从c1到其连通的第i个城市可以带上该城市内的w个救援队
 vector<vector<node>> G; // 邻接表
-vector<int> d, pathNum, teamGather, tempTeam;
-vector<bool> visit;
-// d储存最短路径长度，pathNum储存最短路径数量，teamGather储存能带的队伍数量
-void Dijkstra( int N, int city1 ) { // city1为起始顶点
-    fill( d.begin( ), d.end( ), INF ); // INF意味不可达
-    fill( pathNum.begin( ), pathNum.end( ), 0 );
+vector<int> d, cntOfPath, teamGather, teamOfCity;
+// d储存最短路径长度，cntOfPath储存最短路径数量，teamGather储存能带的队伍数量
+vector<int> visit; // 访问标记
+void Dijkstra( const int &N, const int &start ) {
+    fill( d.begin( ), d.end( ), INF );
+    fill( cntOfPath.begin( ), cntOfPath.end( ), 0 );
     fill( teamGather.begin( ), teamGather.end( ), 0 );
     fill( visit.begin( ), visit.end( ), false );
-    d[city1] = 0; // 赋初值
-    pathNum[city1] = 1;
-    teamGather[city1] = tempTeam[city1];
+    d[start] = 0; // 赋初值
+    cntOfPath[start] = 1;
+    teamGather[start] = teamOfCity[start];
     for( int i = 0; i < N; i++ ) {
-        int u = -1, min = INF;
+        int u = -1, min = INF; // 找离当前城市最近且未访问过的城市
         for( int j = 0; j < N; j++ ) {
             if( visit[j] == false && d[j] < min ) {
                 u = j;
@@ -27,22 +29,20 @@ void Dijkstra( int N, int city1 ) { // city1为起始顶点
             }
         }
         if( u == -1 )
-            return;
-        visit[u] = true; // 容易忘写
-        int size = G[u].size( );
-        for( int j = 0, v = 0; j < size; j++ ) {
-            v = G[u][j].v; // 城市号
+            break;
+        visit[u] = true;
+        for( int j = 0; j < G[u].size( ); j++ ) {
+            int v = G[u][j].v;
             if( visit[v] == false ) {
-                if( d[u] + G[u][j].dis < d[v] ) { // 更新最短路径
+                if( d[u] + G[u][j].dis < d[v] ) { // 更新最短路径长度
                     d[v] = d[u] + G[u][j].dis;
-                    teamGather[v] = teamGather[u] + G[u][j].w;
-                    pathNum[v] = pathNum[u];
-                }
-                else if( d[u] + G[u][j].dis == d[v] ) { // 路径长度相等
-                    if( teamGather[u] + G[u][j].w > teamGather[v] ) { // 且此路径能带的队伍更多
-                        teamGather[v] = teamGather[u] + G[u][j].w;
+                    cntOfPath[v] = cntOfPath[u];
+                    teamGather[v] = teamGather[u] + teamOfCity[v];
+                } else if( d[u] + G[u][j].dis == d[v] ) { // 路径长度相等
+                    if( teamGather[u] + teamOfCity[v] > teamGather[v] ) { // 且此路径能带的队伍更多
+                        teamGather[v] = teamGather[u] + teamOfCity[v];
                     }
-                    pathNum[v] += pathNum[u]; // 更新路径数量
+                    cntOfPath[v] += cntOfPath[u]; // 更新路径数量
                 }
             }
         }
@@ -54,25 +54,18 @@ int main( ) {
     cin >> N >> M >> city1 >> city2;
     G.resize( N );
     d.resize( N );
-    pathNum.resize( N );
+    cntOfPath.resize( N );
     teamGather.resize( N );
+    teamOfCity.resize( N );
     visit.resize( N );
-    for( int i = 0, tempTeamCnt; i < N; i++ ) {
-        cin >> tempTeamCnt;
-        tempTeam.push_back( tempTeamCnt ); // 城市i有tempTeamCnt个救援队
+    for( int i = 0; i < N; i++ )
+        scanf( "%d", &teamOfCity[i] ); // 城市i自己的救援队数
+    for( int i = 0, v1 = 0, v2 = 0, dis = 0; i < M; i++ ) {
+        scanf( "%d %d %d", &v1, &v2, &dis );
+        G[v1].emplace_back( node( v2, dis ) );
+        G[v2].emplace_back( node( v1, dis ) );
     }
-    node temp;
-    for( int i = 0, c1 = 0, c2 = 0, dis = 0; i < M; i++ ) {
-        cin >> c1 >> c2 >> dis;
-        temp.dis = dis;
-        temp.v = c2;
-        temp.w = tempTeam[c2];
-        G[c1].push_back( temp );
-        temp.v = c1;
-        temp.w = tempTeam[c1];
-        G[c2].push_back( temp );
-    } // G[c1][i].v表示c1到其连通的第i个城市(i从0开始)的城市号
-    // G[c1][i].dis表示c1到其连通的第i个城市的距离，G[c1][i].w表示从c1到其连通的第i个城市可以带上该城市内的w个救援队
     Dijkstra( N, city1 );
-    cout << pathNum[city2] << " " << teamGather[city2];
+    cout << cntOfPath[city2] << " " << teamGather[city2];
+    return 0;
 }

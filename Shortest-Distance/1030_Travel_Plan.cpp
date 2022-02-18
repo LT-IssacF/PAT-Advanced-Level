0205c2a71da1dfe1a811d6acd27b9bda05429f88
Jianyang Zhang <82556150+LT-IssacF@users.noreply.github.com>
2022-02-18 16:38:20 +0800

    Update 1030_Travel_Plan.cpp

--- Shortest-Distance/1030_Travel_Plan.cpp
+++ Shortest-Distance/1030_Travel_Plan.cpp
@@ -1,79 +1,68 @@
 #include <iostream>
+#include <cstdio>
 #include <vector>
-#include <algorithm>
 #define INF 0x3fffffff
 using namespace std;
 struct node {
-    int v, dis, cost; // 边权有dis和cost两个
+    int v, dis, cost;
+    node( const int &v2, const int &dis2, const int &cost2 ) : v( v2 ), dis( dis2 ), cost( cost2 ) { }
 };
-vector<vector<node>> G; // 邻接表
-vector<int> d, c, pre; // 分别是最短路径，花费，和保存的路径
-vector<bool> visit; // 访问标记数组
-
-void Dijkstra( int N, int S ) { // S为开始顶点
+vector<vector<node>> G;
+vector<int> d, cost, pre; // 因为只有唯一一条路径，所以前驱数组用一维的就行
+vector<bool> visit;
+void Dijkstra( const int &start, const int &N ) {
     fill( d.begin( ), d.end( ), INF );
-    fill( c.begin( ), c.end( ), INF );
-    fill( pre.begin( ), pre.end( ), -1 );
+    fill( cost.begin( ), cost.end( ), INF );
     fill( visit.begin( ), visit.end( ), false );
-    d[S] = 0; // 赋初值，不能忘
-    c[S] = 0;
-    for( int i = 0, u = 0, min = 0; i < N; i++ ) { // N个顶点循环N次
-        u = -1, min = INF;
-        for( int j = 0; j < N; j++ ) { // 找到未访问顶点中d[]最小的
-            if( visit[j] == false && d[j] < min ) {
+    d[start] = cost[start] = 0;
+    for( int i = 0; i < N; i++ ) {
+        int u = -1, min = INF;
+        for( int j = 0; j < N; j++ )
+            if( d[j] < min && visit[j] == false ) {
                 u = j;
                 min = d[j];
             }
-        }
         if( u == -1 )
-            return;
-        visit[u] = true; // 这句容易忘
-        int size = G[u].size( );
-        for( int j = 0, v = 0; j < size; j++ ) { // 枚举此顶点所能到达的其余顶点
+            break;
+        visit[u] = true;
+        for( int j = 0, v = 0; j < G[u].size( ); j++ ) {
             v = G[u][j].v;
             if( visit[v] == false ) {
-                if( d[u] + G[u][j].dis < d[v] ) { // 更新最短路径
-                    d[v] = d[u] + G[u][j].dis;
-                    c[v] = c[u] + G[u][j].cost;
-                    pre[v] = u; // 前驱
-                }
-                else if( d[u] + G[u][j].dis == d[v] && c[u] + G[u][j].cost < c[v] ) { // 路径长度相同但花费更少
-                    c[v] = c[u] + G[u][j].cost; // 更新代价
-                    pre[v] = u; // 同样的更新前驱
-                }
+                if( d[u] + G[u][j].dis < d[v] ) {
+                    d[v] = d[u] + G[u][j].dis; // 更新最短距离
+                    cost[v] = cost[u] + G[u][j].cost; // 更新代价
+                    pre[v] = u; // 记录前驱
+                } else if( d[u] + G[u][j].dis == d[v] && cost[u] + G[u][j].cost < cost[v] ) {
+                    cost[v] = cost[u] + G[u][j].cost;
+                    pre[v] = u;
+                } // 距离等同但代价更少
             }
         }
     }
 }
 
-void DFS( int S, int v ) { // 递归地回溯路径
-    if( S == v ) { // 回溯到出发点了
-        cout << S << " "; // 这一块执行完就开始一层一层解递归了
+void DFS( const int &start, const int &end ) { // 递归地回溯路径
+    if( start == end ) { // 回溯到出发点了
+        printf( "%d", start ); // 这一块执行完就开始一层一层解递归了
         return;
     }
-    DFS( S, pre[v] );
-    cout << v << " "; // 由深到浅解递归
-} // 这个写法很多要求输出路径的题都可以用
+    DFS( start, pre[end] );
+    printf( " %d", end ); // 由深到浅解递归
+}
 
 int main( ) {
     int N, M, S, D;
     cin >> N >> M >> S >> D;
     G.resize( N );
-    d.resize( N );
-    c.resize( N );
-    pre.resize( N );
+    d.resize( N ), cost.resize( N ), pre.resize( N );
     visit.resize( N );
-    node temp;
-    for( int i = 0, city1 = 0, city2 = 0, tempDis = 0, tempCost = 0; i < M; i++ ) {
-        cin >> city1 >> city2 >> tempDis >> tempCost;
-        temp.v = city2, temp.dis = tempDis, temp.cost = tempCost;
-        G[city1].push_back( temp );
-        temp.v = city1;
-        G[city2].push_back( temp );
-    } // G[city1][i].v表示city1到其连通的第i个城市(i从0开始)的城市号
-    // G[city1][i].dis表示city1到其连通的第i个城市的距离，G[city1][i].cost表示从city1到其连通的第i个城市的花费
-    Dijkstra( N, S );
+    for( int i = 0, v1 = 0, v2 = 0, dis = 0, cost = 0; i < M; i++ ) {
+        scanf( "%d %d %d %d", &v1, &v2, &dis, &cost );
+        G[v1].emplace_back( node( v2, dis, cost ) );
+        G[v2].emplace_back( node( v1, dis, cost ) );
+    }
+    Dijkstra( S, N );
     DFS( S, D );
-    cout << d[D] << " " << c[D];
+    printf( " %d %d", d[D], cost[D] );
     return 0;
 }

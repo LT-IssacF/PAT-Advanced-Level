6e06d56bb9fa60a6e48531c83e08dd4140ea51dc
Jianyang Zhang <82556150+LT-IssacF@users.noreply.github.com>
2022-02-19 17:41:56 +0800

    Create 1036_Boys_vs_Girls.cpp

---
+++ Sort/1036_Boys_vs_Girls.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+struct Record {
+    char gender, name[11], id[11];
+    int grade;
+};
+
+int main( ) {
+    int N;
+    cin >> N;
+    vector<Record> stu( N );
+    for( int i = 0; i < N; i++ )
+        scanf( "%s %c %s %d", &stu[i].name, &stu[i].gender, &stu[i].id, &stu[i].grade );
+    sort( stu.begin( ), stu.end( ), [] ( const Record &a, const Record &b ) {
+        if( a.gender != b.gender ) {
+            return a.gender < b.gender;
+        } else {
+            return a.grade > b.grade;
+        }
+    } );
+    if( stu[0].gender != 'F' ) {
+        printf( "Absent\n" );
+    } else {
+        printf( "%s %s\n", stu[0].name, stu[0].id );
+    } if( stu.back( ).gender != 'M' ) {
+        printf( "Absent\n" );
+    } else {
+        printf( "%s %s\n", stu.back( ).name, stu.back( ).id );
+    } if( stu[0].gender != 'F' || stu.back( ).gender != 'M' ) {
+        printf( "NA\n" );
+    } else {
+        printf( "%d\n", stu[0].grade - stu.back( ).grade );
+    }
+    return 0;
+}

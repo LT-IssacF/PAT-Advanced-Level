6cb5521798709ec3f7b56c934729e6184a5a99f6
Jianyang Zhang <82556150+LT-IssacF@users.noreply.github.com>
2022-02-19 17:42:42 +0800

    Create 1038_Recover_the_Smallest_Number.cpp

---
+++ Sort/1038_Recover_the_Smallest_Number.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main( ) {
+    int N;
+    cin >> N;
+    vector<string> v;
+    string temp, ans;
+    for( int i = 0; i < N; i++ ) {
+        cin >> temp;
+        v.emplace_back( temp );
+    }
+    sort( v.begin( ), v.end( ), [] ( const string &a, const string &b ) {
+        return a + b < b + a;
+    } );
+    for( int i = 0; i < N; i++ )
+        ans += v[i];
+    while( ans.length( ) > 1 && ans[0] == '0' )
+        ans.erase( ans.begin( ) );
+    cout << ( ans.length( ) > 0 ? ans : 0 );
+    return 0;
+}

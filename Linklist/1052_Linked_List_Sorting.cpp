7b24a425c1a98828f8ed7fbbaf96b39f56a95744
Jianyang Zhang <82556150+LT-IssacF@users.noreply.github.com>
2022-02-20 20:20:54 +0800

    Update 1052_Linked_List_Sorting.cpp

--- Linklist/1052_Linked_List_Sorting.cpp
+++ Linklist/1052_Linked_List_Sorting.cpp
@@ -1,51 +1,37 @@
 #include <iostream>
 #include <cstdio>
+#include <vector>
 #include <algorithm>
 using namespace std;
-struct Node {
-    int Address, Data, Next;
-    bool Flag; // Flag标记结点是否在链上
-    Node( ) {
-        Flag = false;
-    }
-} Temp[100020];
-
-bool cmp( Node a, Node b ) {
-    if( !a.Flag || !b.Flag )
-        return a.Flag > b.Flag; // 不在链上的排最后
-    else
-        return a.Data < b.Data;
-}
+struct node {
+    int address, data, next;
+    bool flag = false; // 标记结点是否在链上
+} list[100020];
 
 int main( ) {
-    int N, head;
-    cin >> N >> head;
-    for( int i = 0, Temp_Add = 0; i < N; i++ ) {
-        cin >> Temp_Add;
-        Temp[Temp_Add].Address = Temp_Add;
-        cin >> Temp[Temp_Add].Data >> Temp[Temp_Add].Next;
+    int N, start, cnt = 0;
+    cin >> N >> start;
+    for( int i = 0, address = 0; i < N; i++ ) {
+        scanf( "%d", &address );
+        scanf( "%d %d", &list[address].data, &list[address].next );
+        list[address].address = address;
     }
-    int p = head, cnt = 0;
-    while( p != -1 ) { // 遍历链表，标记哪些结点在链上
-        Temp[p].Flag = true;
-        p = Temp[p].Next;
-        cnt++;
-    }
-    if( cnt == 0 )
-        cout << "0 -1";
-    else {
-        sort( Temp, Temp + 100020, cmp );
-        head = Temp[0].Address;
-        for( int i = 0; i < cnt - 1; i++ ) // 更改结点的Next域
-            Temp[i].Next = Temp[i + 1].Address;
-        Temp[cnt - 1].Next = -1;
-        printf( "%d %05d", cnt, Temp[0].Address );
-        for( int i = 0; i < cnt; i++ ) {
-            if( Temp[i].Next == -1 )
-                printf( "%05d %d -1", Temp[i].Address, Temp[i].Data );
-            else
-                printf( "%05d %d %05d\n", Temp[i].Address, Temp[i].Data, Temp[i].Next );
-        }
+    for( int p = start; p != -1; p = list[p].next, cnt++ ) // 遍历链表，标记哪些结点在链上
+        list[p].flag = true;
+    if( cnt == 0 ) {
+        printf( "0 -1" );
+    } else {
+        sort( list, list + 100020, [] ( const node &a, const node &b ) {
+            if( a.flag != b.flag ) {
+                return a.flag > b.flag; // 不在链上的往后排
+            } else {
+                return a.data < b.data;
+            }
+        } );
+        printf( "%d %05d\n", cnt, list[0].address );
+        for( int i = 0; i < cnt - 1; i++ ) // 本结点的next就是下一个结点的address
+            printf( "%05d %d %05d\n", list[i].address, list[i].data, list[i + 1].address );
+        printf( "%05d %d -1", list[cnt - 1].address, list[cnt - 1].data );
     }
     return 0;
 }

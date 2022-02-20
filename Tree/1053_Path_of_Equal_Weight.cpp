994fe88b4b9bb40a7964248a71f99f68b5ebf49e
Jianyang Zhang <82556150+LT-IssacF@users.noreply.github.com>
2022-02-20 22:03:38 +0800

    Update 1053_Path_of_Equal_Weight.cpp

--- Tree/1053_Path_of_Equal_Weight.cpp
+++ Tree/1053_Path_of_Equal_Weight.cpp
@@ -1,84 +1,53 @@
 #include <iostream>
-#include <cstring>
+#include <cstdio>
 #include <vector>
 #include <algorithm>
 #define MAX 0x7f
 using namespace std;
-struct node { // 邻接表
-    int data, weight;
-    vector<int> child;
+struct node {
+    vector<int> children;
 } tree[MAX];
-vector<int> pre; // 用来保存路径
-// 字符串转换为整型
-int GetData( char tempData[] ) {
-    int data = 0;
-    for( int i = 1, exponent = 1; i >= 0; i-- ) {
-        data += ( tempData[i] - '0' ) * exponent;
-        exponent *= 10;
-    }
-    return data;
-}
-
-bool cmp( int a, int b ) {
-    return  tree[a].weight > tree[b].weight;
-}
- // 这个递归记牢，静态写法经常用
-void Print( int now, int root ) {
-    if( now == root ) {
-        cout << tree[root].weight;
-        return;
-    }
-    Print( pre[now], root );
-    cout << " " << tree[now].weight;
-}
-
-void DFS( int nowNode, int fatherNode, int sum, int S ) {
-    sum += tree[nowNode].weight;
-    int size = tree[nowNode].child.size( );
-    if( sum > S ) // 超过S直接返回
-        return;
-    else if( sum == S ) {
-        if( size > 0 ) // 非叶结点也直接返回
-            return;
-        else { // 满足条件
-            pre[nowNode] = fatherNode; // 保存最后一个结点
-            Print( nowNode, tree[0].data );
-            cout << '\n';
+vector<vector<int>> path; // 储存所有路径
+vector<int> tempPath; // 储存临时路径
+void DFS( const int &now, const int &total, const int &S, const vector<int> &weight ) {
+    if( tree[now].children.size( ) == 0 ) { // 到达叶结点了
+        if( total + weight[now] == S ) { // 如果祖先的权值之和加上自己的等于S
+            tempPath.push_back( weight[now] );
+            path.emplace_back( tempPath );
+            tempPath.pop_back( ); // 回溯
         }
+        return;
     }
-    else {
-        for( int i = 0; i < size; i++ ) { // 深度遍历当前结点的孩子
-            pre[tree[nowNode].child[i]] = nowNode;
-            DFS( tree[nowNode].child[i], nowNode, sum, S );
-        } // pre数组其实也保存了整个树的父子结构关系除了因大于S而提前结束的那一部分树
-    } // 只是需要使用其中一条符合条件的路径
-} // 也就是说pre数组不光是保存结果的，结果只是pre数组里的一部分
+    tempPath.push_back( weight[now] );
+    for( int i = 0; i < tree[now].children.size( ); i++ ) // 深度遍历当前结点的孩子
+        DFS( tree[now].children[i], total + weight[now], S, weight );
+    tempPath.pop_back( ); // 回溯
+}
 
 int main( ) {
     int N, M, S;
     cin >> N >> M >> S;
-    pre.resize( N );
-    pre[0] = 0; // 根结点处理
+    vector<int> weight( N );
     for( int i = 0; i < N; i++ )
-        tree[i].data = i;
-    vector<int> tempWeight;
-    for( int i = 0, w = 0; i < N; i++ ) {
-        cin >> w; // 权重是按结点序号给的
-        tempWeight.push_back( w );
+        scanf( "%d", &weight[i] );
+    for( int i = 0, id = 0, K = 0; i < M; i++ ) {
+        scanf( "%d %d", &id, &K );
+        for( int j = 0, child = 0; j < K; j++ ) {
+            scanf( "%d", &child );
+            tree[id].children.push_back( child );
+        }
+        sort( tree[id].children.begin( ), tree[id].children.end( ),
+            [weight] ( const int &a, const int &b ) {
+            return weight[a] > weight[b];
+        } ); // 没输完一个非叶结点就将它的孩子按权值降序排列
     }
-    tree[0].weight = tempWeight[0]; // 根结点处理
-    char tempData[3];
-    for( int i = 0, k = 0, nodeData; i < M; i++ ) {
-        cin >> tempData >> k;
-        nodeData = GetData( tempData );
-        for( int j = 0, childData = 0; j < k; j++ ) {
-            cin >> tempData;
-            childData = GetData( tempData );
-            tree[nodeData].child.push_back( childData );
-            tree[childData].weight = tempWeight[childData];
-        } // 把每个结点的孩子按weight从大到小排才能符合题目要求
-        sort( tree[nodeData].child.begin( ), tree[nodeData].child.end( ),cmp );
+    DFS( 0, 0, S, weight );
+    sort( path.begin( ), path.end( ), greater<vector<int>>( ) ); // 当一个结点的孩子中有多个孩子权值相等
+    for( int i = 0; i < path.size( ); i++ ) { // 而后面的结点又不能保证有序，就会出错，所以要对所有路径集中排序
+        printf( "%d", path[i][0] );
+        for( int j = 1; j < path[i].size( ); j++ )
+            printf( " %d", path[i][j] );
+        printf( "\n" );
     }
-    DFS( 0, 0, 0, S );
     return 0;
 }

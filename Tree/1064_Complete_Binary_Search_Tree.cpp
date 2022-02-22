037fd24f89459420505b81ec38c909fbda185925
Jianyang Zhang <82556150+LT-IssacF@users.noreply.github.com>
2022-02-22 17:38:33 +0800

    Update 1064_Complete_Binary_Search_Tree.cpp

--- Tree/1064_Complete_Binary_Search_Tree.cpp
+++ Tree/1064_Complete_Binary_Search_Tree.cpp
@@ -1,28 +1,25 @@
 #include <iostream>
+#include <cstdio>
 #include <algorithm>
-#define MAX 1020
 using namespace std;
-int tree[MAX] = { -1 }, arr[MAX] = { -1 };
-// 类似1099，不同的是这道的是从0开始连续递增所以更方便
-// 一个二叉搜索树的中序序列是递增的，中序遍历时将数组挨个插入
-void InOrder( int root, int N, int &index ) {
+int arr[1020] = { 0 }, tree[1020] = { 0 };
+void InOrder( int &now, const int &root, const int &N ) {
     if( root > N )
         return;
-    InOrder( root * 2, N, index );
-    tree[root] = arr[index++];
-    InOrder( root * 2 + 1, N, index );
+    InOrder( now, root * 2, N );
+    tree[root] = arr[now++];
+    InOrder( now, root * 2 + 1, N );
 }
-
+// 中序遍历按数组顺序挨个插入即可
 int main( ) {
-    int N;
+    int N, now = 0;
     cin >> N;
     for( int i = 0; i < N; i++ )
-        cin >> arr[i];
-    sort( arr, arr + N );
-    int index = 0;
-    InOrder( 1, N, index );
+        scanf( "%d", &arr[i] );
+    sort( arr, arr + N ); // 二叉搜索树的中序序列是严格递增的
+    InOrder( now, 1, N ); // root不能设为0
     cout << tree[1];
     for( int i = 2; i <= N; i++ )
-        cout << " " << tree[i];
+        printf( " %d", tree[i] );
     return 0;
 }

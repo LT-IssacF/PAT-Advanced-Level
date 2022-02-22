070e0aab512aaebc0504674d2ab2c54d126bfed4
Jianyang Zhang <82556150+LT-IssacF@users.noreply.github.com>
2022-02-22 20:24:30 +0800

    Update 1066_Root_of_AVL_Tree.cpp

--- Tree/1066_Root_of_AVL_Tree.cpp
+++ Tree/1066_Root_of_AVL_Tree.cpp
@@ -1,28 +1,20 @@
 #include <iostream>
+#include <cstdio>
+#include <algorithm>
 using namespace std;
 typedef struct BiNode {
     int data, height;
-    struct BiNode *lchild, *rchild;
+    BiNode *lchild, *rchild;
 } BiNode, *BiTree;
-
-BiTree NewNode( int key ) {
-    BiTree T = ( BiTree ) malloc( sizeof( struct BiNode ) );
-    T->data = key;
-    T->height = 1;
-    T->lchild = T->rchild = NULL;
-    return T;
-}
-// 其实我觉得下面这几个高度相关的操作递归理解起来比左旋右旋难
-int GetHeight( BiTree T ) {
-    if( !T )
-        return 0;
-    return T->height;
+int GetHeight( const BiTree &T ) {
+    return ( T == NULL ? 0 : T->height );
 }
 // 平衡因子
-int GetBalence( BiTree T ) {
+int GetBalance( const BiTree &T ) {
     return GetHeight( T->lchild ) - GetHeight( T->rchild );
 }
-void UpdateHeight( BiTree T ) {
+// 不要忘了+1
+void UpdateHeight( BiTree &T ) {
     T->height = max( GetHeight( T->lchild ), GetHeight( T->rchild ) ) + 1;
 }
 // 左旋
@@ -43,49 +35,45 @@ void RightRotation( BiTree &T ) {
     UpdateHeight( temp );
     T = temp;
 }
-// LL和LR型树都是左高右低，RR和RL型树是右高左低
-// LL(2,1)是root的左子树左边高，而LR(2,-1)是root的左子树右边高
-// 所以LL型一次右旋就可以了，而LR型要先对root的左子树左旋，再对root右旋
-// RR和RL型同理
-void Insert( BiTree &T, int key ) {
-    if( !T ) {
-        T = NewNode( key );
+
+void Insert( BiTree &T, const int &key ) {
+    if( T == NULL ) {
+        T = ( BiTree ) malloc( sizeof( BiNode ) );
+        T->data = key, T->height = 1;
+        T->lchild = T->rchild = NULL;
         return;
     }
     if( T->data > key ) { // 往左边插
         Insert( T->lchild, key );
         UpdateHeight( T );
-        if( GetBalence( T ) == 2 ) {
-            if( GetBalence( T->lchild ) == 1 ) { // LL型树
+        if( GetBalance( T ) == 2 ) { // 需要平衡
+            if( GetBalance( T->lchild ) == 1 ) { // LL型树
                 RightRotation( T ); // 一次右旋
-            }
-            else if( GetBalence( T->lchild ) == -1 ) { // LR型树
-                LeftRotation( T->lchild ); // 先对左子树左旋
+            } else if( GetBalance( T->lchild ) == -1 ) { // LR型树
+                LeftRotation( T->lchild ); // 先对左子树左旋旋
                 RightRotation( T ); // 再给自己右旋
             }
         }
-    }
-    else if( T->data < key ) { // 往右边插
+    } else { // 往右边插
         Insert( T->rchild, key );
         UpdateHeight( T );
-        if( GetBalence( T ) == -2 ) {
-            if( GetBalence( T->rchild ) == -1 ) { // RR型树
+        if( GetBalance( T ) == -2 ) { // 需要平衡
+            if( GetBalance( T->rchild ) == -1 ) { // RR型树
                 LeftRotation( T ); // 一次左旋
-            }
-            else if( GetBalence( T->rchild ) == 1 ) { // RL型树
+            } else if( GetBalance( T->rchild ) == 1 ) { // RL型树
                 RightRotation( T->rchild ); // 先对右子树右旋
                 LeftRotation( T ); // 再对自己左旋
-            }
-        }
-    }
-}
+            } // LL和LR型树都是左高右低，RR和RL型树是右高左低
+        } // LL(2,1)是root的左子树左边高，而LR(2,-1)是root的左子树右边高
+    } // 所以LL型一次右旋就可以了，而LR型要先对root的左子树左旋，再对root右旋
+} // RR和RL型同理
 
 int main( ) {
     int N;
     cin >> N;
     BiTree T = NULL;
     for( int i = 0, key = 0; i < N; i++ ) {
-        cin >> key;
+        scanf( "%d", &key );
         Insert( T, key );
     }
     cout << T->data;

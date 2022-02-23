1b0e3469f433d6c4336a630a58a3347a766d76fa
Jianyang Zhang <82556150+LT-IssacF@users.noreply.github.com>
2022-02-23 21:25:17 +0800

    Update 1074_Reversing_Linked_List.cpp

--- Linklist/1074_Reversing_Linked_List.cpp
+++ Linklist/1074_Reversing_Linked_List.cpp
@@ -1,51 +1,32 @@
-#include <iostream>
-#include <cstdio>
-#include <vector>
-#include <algorithm>
-using namespace std;
-struct node {
-    int Address, Data, Next;
-    int Num; // 结点在链表中的序号
-    node( ) {
-        Num = 0x7fffffff;
-    }
-} list[100020];
-
-bool cmp( node a, node b ) {
-    return a.Num < b.Num;
-}
-
 int main( ) {
-    int head, N, K;
-    cin >> head >> N >> K;
+    int start, N, K;
+    cin >> start >> N >> K;
     for( int i = 0, address = 0; i < N; i++ ) {
-        cin >> address;
-        cin >> list[address].Data >> list[address].Next;
-        list[address].Address = address;
+        scanf( "%d", &address );
+        scanf( "%d %d", &list[address].data, &list[address].next );
+        list[address].address = address;
     }
     int cnt = 0;
-    while( head != -1 ) { // 遍历单链表
-        list[head].Num = cnt++;
-        head = list[head].Next;
-    }
-    sort( list, list + 100020, cmp ); // 不在链表上的无效结点排在最后
-    int round = cnt / K, remainder = cnt % K; // round为需要逆转的组数，remainder为最后不构成一组的结点
-    // 链表结构本身不需要逆转，只需要从每组最后一个结点开始倒着输出一个小组的地址域
-    // 直到这个组的第一个结点需要做特殊处理，即最后一个结点的Next域需要根据当前组数判断是否正常输出下个组的最后一个结点的地址域
-    for( int i = 0, j = 0, k = 0; i < round; i++ ) {
-        for( j = ( i + 1 ) * K - 1; j >= i * K + 1; j-- )
-            printf( "%05d %d %05d\n", list[j].Address, list[j].Data, list[j - 1].Address );
-        printf( "%05d %d", list[j].Address, list[j].Data );
-        if( i < round - 1 ) // 不是最后那组，正常输出逆转小组的下一个小组的最后一个结点的地址域
-            printf( " %05d\n", list[( i + 2 ) * K - 1].Address );
-        else { // 非常情况
-            if( remainder == 0 ) // 正好分完每个组，没有余下的结点
-                cout << " -1";
-            else { // 余下的不用逆转，从前往后正常输出
-                printf( " %05d\n", list[( i + 1 ) * K].Address );
-                for( k = ( i + 1 ) * K; k < cnt - 1; k++ )
-                    printf( "%05d %d %05d\n", list[k].Address, list[k].Data, list[k + 1].Address );
-                printf( "%05d %d -1", list[k].Address, list[k].Data );
+    for( int p = start; p != -1; p = list[p].next )
+        list[p].order = cnt++;
+    sort( list, list + 100020, [] ( const node &a, const node &b ) {
+        return a.order < b.order;
+    } ); // 不在链表上的无效结点排在最后
+    int block = cnt / K, remnant = cnt % K; // block为需要逆转的块数，remnant为最后剩下不构成一组无需逆转的结点数
+    for( int i = 0; i < block; i++ ) {
+        for( int j = ( i + 1 ) * K - 1; j > i * K; j-- )
+            printf( "%05d %d %05d\n", list[j].address, list[j].data, list[j - 1].address );
+        printf( "%05d %d", list[i * K].address, list[i * K].data );
+        if( i < block - 1 ) { // 不是最后那组，正常逆转输出下一块最后一个结点的地址域
+            printf( " %05d\n", list[( i + 2 ) * K - 1].address );
+        } else {
+            if( remnant == 0 ) { // 正好分完每个组，没有余下的结点
+                printf( " -1" );
+            } else { // 余下的不用逆转，从前往后正常输出
+                printf( " %05d\n", list[( i + 1 ) * K].address );
+                for( int j = ( i + 1 ) * K; j < cnt - 1; j++ )
+                    printf( "%05d %d %05d\n", list[j].address, list[j].data, list[j + 1].address );
+                printf( "%05d %d -1", list[cnt - 1].address, list[cnt - 1].data );
             }
         }
     }

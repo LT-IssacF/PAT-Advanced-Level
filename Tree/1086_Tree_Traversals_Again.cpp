44156003c07de02d4c12a121bd5ea146d2bb6dea
Jianyang Zhang <82556150+LT-IssacF@users.noreply.github.com>
2022-02-25 16:54:59 +0800

    Update 1086_Tree_Traversals_Again.cpp

--- Tree/1086_Tree_Traversals_Again.cpp
+++ Tree/1086_Tree_Traversals_Again.cpp
@@ -1,60 +1,57 @@
 #include <iostream>
-#include <string>
+#include <cstdio>
 #include <vector>
 #include <stack>
 using namespace std;
 typedef struct BiNode {
-    int Data;
-    struct BiNode *Left, *Right;
+    int data;
+    BiNode *lchild, *rchild;
 } BiNode, *BiTree;
 vector<int> pre, in, post;
-// 这题没啥好讲的，就是输入用了栈，其他在前面的遍历那说过了
-BiTree Build_Tree( int preL, int preR, int inL, int inR ) {
+BiTree Insert( const int &preL, const int &preR, const int &inL, const int &inR ) {
     if( preL > preR )
         return NULL;
     int i, numLeft;
-    for( i = 0; i <= preR; i++ )
-        if( in[i] == pre[preL] )
+    for( i = inL; i <= inR; i++ )
+        if( in[i] == pre[preL] ) {
+            numLeft = i - inL;
             break;
-    numLeft = i - inL;
-    BiTree T = ( BiTree ) malloc( sizeof( struct BiNode ) );
-    T->Data = pre[preL];
-    T->Left = Build_Tree( preL + 1, preL + numLeft, inL, i - 1 );
-    T->Right = Build_Tree( preL + numLeft + 1, preR, i + 1, inR );
+        }
+    BiTree T = ( BiTree ) malloc( sizeof( BiNode ) );
+    T->data = pre[preL];
+    T->lchild = Insert( preL + 1, preL + numLeft, inL, i - 1 );
+    T->rchild = Insert( preL + numLeft + 1, preR, i + 1, inR );
     return T;
 }
 
-void Traversals( BiTree T ) {
-    if( !T )
+void PostOrder( const BiTree &T ) {
+    if( T == NULL )
         return;
-    Traversals( T->Left );
-    Traversals( T->Right );
-    post.push_back( T->Data );
+    PostOrder( T->lchild );
+    PostOrder( T->rchild );
+    post.emplace_back( T->data );
 }
 
 int main( ) {
     int N;
     cin >> N;
     stack<int> s;
-    string order;
-    for( int i = 0, temp = 0; i < 2 * N; i++ ) {
+    string order, push = "Push";
+    for( int i = 0, key = 0; i < N * 2; i++ ) {
         cin >> order;
-        if( order == "Push" ) {
-            cin >> temp;
-            pre.push_back( temp );
-            s.push( temp );
-        }
-        else {
+        if( order == push ) {
+            scanf( "%d", &key );
+            pre.push_back( key );
+            s.push( key );
+        } else {
             in.push_back( s.top( ) );
             s.pop( );
         }
     }
-    BiTree T = Build_Tree( 0, N - 1, 0, N - 1 );
-    Traversals( T );
-    int size = post.size( );
-    if( size > 0 )
-        cout << post[0];
-    for( int i = 1; i < size; i++ )
-        cout << " " << post[i];
+    BiTree T = Insert( 0, N - 1, 0, N - 1 );
+    PostOrder( T );
+    printf( "%d", post[0] );
+    for( int i = 1; i < N; i++ )
+        printf( " %d", post[i] );
     return 0;
 }

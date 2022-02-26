44ed14491bbc8f3a5640a6408e01a892f3e7579a
Jianyang Zhang <82556150+LT-IssacF@users.noreply.github.com>
2022-02-26 13:37:43 +0800

    Update 1090_Highest_Price_in_Supply_Chain.cpp

--- Tree/1090_Highest_Price_in_Supply_Chain.cpp
+++ Tree/1090_Highest_Price_in_Supply_Chain.cpp
@@ -1,42 +1,38 @@
 #include <iostream>
 #include <cstdio>
-#include <cmath>
 #include <vector>
+#include <cmath>
 using namespace std;
 vector<vector<int>> tree;
-// 这题就是供应链那道的青春版
-void DFS( int root, int layer, int &maxLevel, int &cnt ) {
-    int size = tree[root].size( );
-    if( size == 0 ) { // 到零售商了
-        if( layer > maxLevel ) { // 经经销商更多的零售商
-            maxLevel = layer;
-            cnt = 1;
-        }
-        else if( layer == maxLevel ) { // 不止一个
+void DFS( const int &now, const int &depth, int &deepest, int &cnt ) {
+    if( tree[now].size( ) == 0 ) { // 到零售商了
+        if( depth > deepest ) {
+            deepest = depth; // 更新深度
+            cnt = 1; // 计数重置
+        } else if( depth == deepest ) {
             cnt++;
         }
         return;
     }
-    for( int i = 0; i < size; i++ ) {
-        DFS( tree[root][i], layer + 1, maxLevel, cnt );
-    }
+    for( int i = 0; i < tree[now].size( ); i++ )
+        DFS( tree[now][i], depth + 1, deepest, cnt );
 }
 
 int main( ) {
-    int N, root;
+    int N, root, deepest = 0, cnt = 1;
     double P, r;
     cin >> N >> P >> r;
     tree.resize( N );
     r /= 100;
-    for( int i = 0, father = 0; i < N; i++ ) {
-        cin >> father;
-        if( father != -1 )
-            tree[father].push_back( i );
-        else
+    for( int i = 0, supplier = 0; i < N; i++ ) {
+        scanf( "%d", &supplier );
+        if( supplier >= 0 ) {
+            tree[supplier].push_back( i );
+        } else {
             root = i;
+        }
     }
-    int maxLevel = 0, cnt = 0;
-    DFS( root, 0, maxLevel, cnt );
-    printf( "%.2f %d", P * pow( 1 + r, maxLevel ), cnt );
+    DFS( root, 1, deepest, cnt );
+    printf( "%.2f %d", P * pow( 1 + r, deepest - 1 ), cnt );
     return 0;
 }

cba67cd4170f3aa08c36d2c0082d3162cde636a2
Jianyang Zhang <82556150+LT-IssacF@users.noreply.github.com>
2022-02-26 15:58:46 +0800

    Update 1093_Count_PAT's.cpp

--- Iteration/1093_Count_PAT's.cpp
+++ Iteration/1093_Count_PAT's.cpp
@@ -4,19 +4,18 @@
 using namespace std;
 
 int main( ) {
+    int ans = 0;
     string s;
     cin >> s;
-    int length = s.length( ), rightT = 0, ans = 0; // rightT统计当前字符右边T的个数
-    vector<int> leftP( length ); // 统计当前字符左边'P'的个数
-    leftP[0] = 0; // 边界
-    for( int i = 1; i < length; i++ )
-        leftP[i] = ( s[i - 1] == 'P' ) ? leftP[i - 1] + 1 : leftP[i - 1];
-    for( int i = length - 1; i >= 0; i-- ) {
-        if( s[i] == 'T' )
-            rightT++;
-        else if( s[i] == 'A' )
+    vector<int> leftP( s.length( ), 0 ); // 统计当前字符左边P的个数
+    for( int i = 1; i < s.length( ); i++ )
+        leftP[i] = s[i - 1] == 'P' ? leftP[i - 1] + 1 : leftP[i - 1];
+    for( int i = s.length( ) - 1, rightT = 0; i >= 0; i-- ) // rightT统计当前字符右边T的个数
+        if( s[i] == 'A' ) {
             ans = ( ans + leftP[i] * rightT ) % 1000000007;
-    }
-    cout << ans;
+        } else if( s[i] == 'T' ) {
+            rightT++;
+        }
+    printf( "%d", ans );
     return 0;
 }

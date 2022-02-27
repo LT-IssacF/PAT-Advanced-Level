ec6298a35ca92b4c939b7a19bfc7171cf3b4ffdb
Jianyang Zhang <82556150+LT-IssacF@users.noreply.github.com>
2022-02-27 13:18:13 +0800

    Update 1099_Build_A_Binary_Search_Tree.cpp

--- Tree/1099_Build_A_Binary_Search_Tree.cpp
+++ Tree/1099_Build_A_Binary_Search_Tree.cpp
@@ -1,55 +1,48 @@
 #include <iostream>
+#include <cstdio>
 #include <vector>
 #include <queue>
 #include <algorithm>
-#define MAX 0x7f
 using namespace std;
 struct node {
-    int data, left, right;
-} tree[MAX];
-vector<int> in, level;
-// 跟1064一个思路，利用中序遍历是关键
-// 二叉搜索树的中序序列就是递增序列，利用此性质
-void InOrder( int root, int &key ) {
-    if( root == -1 )
+    int data, lchild, rchild;
+} tree[0x7f];
+int p = 0; // 在in中的索引
+void InOrder( const int &now, const vector<int> &in ) {
+    if( now == -1 )
         return;
-    InOrder( tree[root].left, key );
-    tree[root].data = in[key++];
-    InOrder( tree[root].right, key );
-}
+    InOrder( tree[now].lchild, in );
+    tree[now].data = in[p++];
+    InOrder( tree[now].rchild, in );
+} // 二叉搜索树的中序序列是递增序列，利用此性质
 
-void LayerOrder( int root ) {
+void BFS( const int &root, vector<int> &ans ) {
     queue<int> q;
     q.push( root );
     while( !q.empty( ) ) {
-        root = q.front( );
-        level.push_back( tree[root].data );
+        int front = q.front( );
+        ans.push_back( tree[front].data );
         q.pop( );
-        if( tree[root].left != -1 )
-            q.push( tree[root].left );
-        if( tree[root].right != -1 )
-            q.push( tree[root].right );
+        if( tree[front].lchild >= 0 )
+            q.push( tree[front].lchild );
+        if( tree[front].rchild >= 0 )
+            q.push( tree[front].rchild );
     }
 }
 
 int main( ) {
     int N;
     cin >> N;
-    for( int i = 0, tempLeft = 0, tempRight = 0; i < N; i++ ) {
-        cin >> tempLeft >> tempRight;
-        tree[i].left = tempLeft;
-        tree[i].right = tempRight;
-    }
-    for( int i = 0, temp = 0; i < N; i++ ) {
-        cin >> temp;
-        in.push_back( temp );
-    }
+    for( int i = 0; i < N; i++ )
+        scanf( "%d %d", &tree[i].lchild, &tree[i].rchild );
+    vector<int> in( N ), ans;
+    for( int i = 0; i < N; i++ )
+        scanf( "%d", &in[i] );
     sort( in.begin( ), in.end( ) );
-    int key = 0;
-    InOrder( 0, key );
-    LayerOrder( 0 );
-    cout << level[0];
+    InOrder( 0, in );
+    BFS( 0, ans );
+    printf( "%d", ans[0] );
     for( int i = 1; i < N; i++ )
-        cout << " " << level[i];
+        printf( " %d", ans[i] );
     return 0;
 }

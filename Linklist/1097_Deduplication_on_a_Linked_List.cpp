2d8673d18fdde3f6f8e51439aa953349fc01e169
Jianyang Zhang <82556150+LT-IssacF@users.noreply.github.com>
2022-02-27 12:44:08 +0800

    Update 1097_Deduplication_on_a_Linked_List.cpp

--- Linklist/1097_Deduplication_on_a_Linked_List.cpp
+++ Linklist/1097_Deduplication_on_a_Linked_List.cpp
@@ -1,46 +1,38 @@
 #include <iostream>
 #include <cstdio>
+#include <cmath>
 #include <algorithm>
-#define MAX 0x1fffffff
+#define INF 0x3fffffff
 using namespace std;
 struct node {
-    int address, data, next, order;
-    node( ) { // 初始顺序为2 * INF
-        order = 2 * MAX;
-    }
+    int address, data, next, order = INF * 2; // 初始顺序为INF * 2
 } list[100020];
-bool visit[10020] = { false }; // 相当于哈希表
+bool hashTable[100020] = { false };
 
 int main( ) {
-    int start, N;
+    int start, N, validCnt = 0, total = 0;
     cin >> start >> N;
-    for( int i = 0, a = 0; i < N; i++ ) {
-        cin >> a;
-        cin >> list[a].data >> list[a].next;
-        list[a].address = a;
+    for( int i = 0, address = 0; i < N; i++ ) {
+        scanf( "%d", &address );
+        scanf( "%d %d", &list[address].data, &list[address].next );
+        list[address].address = address;
     }
-    int s = start, cnt = 0, validCnt = 0, delNum = MAX; // 要删除的多余重复结点的顺序从MAX开始重新编号
-    while( s != -1 ) { // 遍历单链表
-        if( visit[abs( list[s].data )] == false ) { // data第一次出现
-            list[s].order = ++validCnt; // 标记结点在链表中的顺序
-            visit[abs( list[s].data )] = true; // 标记data
+    for( int p = start, delCnt = 0; p != -1; p = list[p].next, total++ ) // 遍历单链表
+        if( hashTable[abs( list[p].data )] == false ) { // data第一次出现
+            list[p].order = validCnt++;
+            hashTable[abs( list[p].data )] = true;
+        } else { // 重复的
+            list[p].order = INF + delCnt; // 要删除的多余重复结点的顺序从INF开始重新编号
+            delCnt++;
         }
-        else // 重复的
-            list[s].order = ++delNum;
-        cnt++; // 在链表上的结点的个数
-        s = list[s].next;
-    }
-    sort( list, list + 100020,
-        [] ( node a, node b ) {
-            return a.order < b.order;
-        } ); // 在链上的结点从1开始排，重复的从INF开始排，不在链上的排在最后
-    for( int i = 0; i < cnt; i++ ) { // 仅输出在链上的
-        if( i == validCnt - 1 || i == cnt - 1 ) { // 不重复的或重复的的最后一个结点
+    sort( list, list + 100020, [] ( const node &a, const node &b ) {
+        return a.order < b.order;
+    } ); // 在链上的结点从0开始排，重复的从INF开始排，不在链上的排在最后
+    for( int i = 0; i < total; i++ )
+        if( i == validCnt - 1 || i == total - 1 ) {
             printf( "%05d %d -1\n", list[i].address, list[i].data );
-        }
-        else {
+        } else {
             printf( "%05d %d %05d\n", list[i].address, list[i].data, list[i + 1].address );
         }
-    }
     return 0;
 }

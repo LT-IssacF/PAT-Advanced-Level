b9fcd3215ac81629c43567e67226c80629c049a4
Jianyang Zhang <82556150+LT-IssacF@users.noreply.github.com>
2022-03-02 01:02:52 +0800

    Update 1123_Is_It_a_Complete_AVL_Tree.cpp

--- Tree/1123_Is_It_a_Complete_AVL_Tree.cpp
+++ Tree/1123_Is_It_a_Complete_AVL_Tree.cpp
@@ -1,32 +1,22 @@
 #include <iostream>
+#include <cstdio>
+#include <vector>
 #include <queue>
-#include <algorithm>
 using namespace std;
 typedef struct BiNode {
     int data, height;
-    struct BiNode *lchild, *rchild;
+    BiNode *lchild, *rchild;
 } BiNode, *BiTree;
-bool shouldBeComplete = true, startCheck = false;
-BiTree NewNode( int key ) {
-    BiTree T = ( BiTree ) malloc( sizeof( struct BiNode ) );
-    T->data = key;
-    T->height = 1;
-    T->lchild = T->rchild = NULL;
-    return T;
+int GetHeight( const BiTree &T ) {
+    return T == NULL ? 0 : T->height;
 }
 
-int GetHeight( const BiTree &T ) {
-    if( !T )
-        return 0;
-    return T->height;
+int GetBalance( const BiTree &T ) {
+    return GetHeight( T->lchild ) - GetHeight( T->rchild );
 }
 
 void UpdateHeight( BiTree &T ) {
     T->height = max( GetHeight( T->lchild ), GetHeight( T->rchild ) ) + 1;
-}
-
-int GetBalence( const BiTree &T) {
-    return GetHeight( T->lchild ) - GetHeight( T->rchild );
 }
 
 void LeftRotation( BiTree &T ) {
@@ -47,68 +37,72 @@ void RightRotation( BiTree &T ) {
     T = temp;
 }
 
-void Insert( BiTree &T, int key ) {
-    if( !T ) {
-        T = NewNode( key );
+void Insert( BiTree &T, const int &key ) {
+    if( T == NULL ) {
+        T = ( BiTree ) malloc( sizeof( BiNode ) );
+        T->data = key, T->height = 1;
+        T->lchild = T->rchild = NULL;
         return;
     }
-    else if( T->data > key ) {
+    if( T->data > key ) {
         Insert( T->lchild, key );
         UpdateHeight( T );
-        if( GetBalence( T ) == 2 ) {
-            if( GetBalence( T->lchild ) == 1 )
+        if( GetBalance( T ) == 2 ) {
+            if( GetBalance( T->lchild ) == 1 ) {
                 RightRotation( T );
-            else if( GetBalence( T->lchild ) == -1 ) {
+            } else if( GetBalance( T->lchild ) == -1 ) {
                 LeftRotation( T->lchild );
                 RightRotation( T );
             }
         }
-    }
-    else if( T->data < key ) {
+    } else {
         Insert( T->rchild, key );
         UpdateHeight( T );
-        if( GetBalence( T ) == -2 ) {
-            if( GetBalence( T->rchild ) == -1 )
+        if( GetBalance( T ) == -2 ) {
+            if( GetBalance( T->rchild ) == -1 ) {
                 LeftRotation( T );
-            else if( GetBalence( T->rchild ) == 1 ) {
+            } else if( GetBalance( T->rchild ) == 1 ) {
                 RightRotation( T->rchild );
                 LeftRotation( T );
             }
         }
     }
 }
-// 建AVL树在1066中详细说明过，判断一棵树是不是完全二叉树在1110中也说明过
-void BFS( const BiTree &T ) {
+
+bool BFS( const BiTree &T ) {
     queue<BiTree> q;
     q.push( T );
+    bool startToCheck = false, ans = true;
     while( !q.empty( ) ) {
         BiTree front = q.front( );
-        if( front != T )
-            cout << " ";
-        cout << front->data;
+        if( front == T ) {
+            printf( "%d", front->data );
+        } else {
+            printf( " %d", front->data );
+        }
         q.pop( );
-        if( !front->lchild && front->rchild )
-            shouldBeComplete = false;
-        if( startCheck && ( front->lchild || front->rchild ) )
-            shouldBeComplete = false;
-        if( front->lchild )
+        if( front->lchild == NULL && front->rchild != NULL )
+            ans = false;
+        if( startToCheck == true && ( front->lchild != NULL || front->rchild != NULL ) )
+            ans = false;
+        if( front->lchild != NULL )
             q.push( front->lchild );
-        if( front->rchild )
+        if( front->rchild != NULL )
             q.push( front->rchild );
-        if( !front->lchild || !front->rchild )
-            startCheck = true;
+        if( front->lchild == NULL || front->rchild == NULL )
+            startToCheck = true;
     }
+    return ans;
 }
-
+// 建AVL树在1066中详细说明过，判断一棵树是不是完全二叉树在1110中也说明过
 int main( ) {
     int N;
     cin >> N;
     BiTree T = NULL;
     for( int i = 0, key = 0; i < N; i++ ) {
-        cin >> key;
+        scanf( "%d", &key );
         Insert( T, key );
     }
-    BFS( T );
-    cout << endl << ( shouldBeComplete ? "YES" : "NO" );
+    printf( "\n%s", BFS( T ) ? "YES" : "NO" );
     return 0;
 }

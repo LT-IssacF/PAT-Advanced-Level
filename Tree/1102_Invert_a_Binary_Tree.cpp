db60638bbb5c6f621c52b92daa3438da555c7cdb
Jianyang Zhang <82556150+LT-IssacF@users.noreply.github.com>
2022-03-02 13:31:00 +0800

    Update 1102_Invert_a_Binary_Tree.cpp

--- Tree/1102_Invert_a_Binary_Tree.cpp
+++ Tree/1102_Invert_a_Binary_Tree.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <vector>
+#include <cstdio>
 #include <queue>
 #include <algorithm>
 #define MAX 0xf
@@ -7,71 +7,73 @@ using namespace std;
 struct node {
     int lchild, rchild;
 } tree[MAX];
-bool hashTable[MAX] = { false }; // 找根用的
-vector<int> level, in;
-// 字符转整型
-int CharToInt( char s ) {
-    if( s == '-' )
-        return -1;
-    hashTable[( s - '0' )] = true;
-    return s - '0';
-}
-// 没出现过就是根
-int FindRoot( int N ) {
-    for( int i = 0; i < N; i++ )
-        if( hashTable[i] == false )
-            return i;
-    return -1;
-}
-// 利用后序遍历的性质可以对一个二叉树左右调换
-void PostOrder( int root ) {
-    if( root == -1 )
-        return;
-    PostOrder( tree[root].lchild );
-    PostOrder( tree[root].rchild );
-    swap( tree[root].lchild, tree[root].rchild );
-}
-
-void BFS( int root ) {
+vector<int> in, level;
+void BFS( const int &root ) {
     queue<int> q;
     q.push( root );
     while( !q.empty( ) ) {
-        root = q.front( );
-        level.push_back( root );
+        int front = q.front( );
+        level.push_back( front );
         q.pop( );
-        if( tree[root].lchild != -1 )
-            q.push( tree[root].lchild );
-        if( tree[root].rchild != -1 )
-            q.push( tree[root].rchild );
+        if( tree[front].lchild >= 0 )
+            q.push( tree[front].lchild );
+        if( tree[front].rchild >= 0 )
+            q.push( tree[front].rchild );
     }
 }
-
-void InOrder( int root ) {
-    if( root == -1 )
+ // 利用后序遍历的性质可以对一个二叉树左右调换
+void PostOrder( const int &now ) {
+    if( now < 0 )
         return;
-    InOrder( tree[root].lchild );
-    in.push_back( root );
-    InOrder( tree[root].rchild );
+    PostOrder( tree[now].lchild );
+    PostOrder( tree[now].rchild );
+    swap( tree[now].lchild, tree[now].rchild );
+}
+
+void InOrder( const int &now ) {
+    if( now < 0 )
+        return;
+    InOrder( tree[now].lchild );
+    in.push_back( now );
+    InOrder( tree[now].rchild );
+}
+
+void Print( const int &N, const vector<int> &sequence ) {
+    printf( "%d", sequence[0] );
+    for( int i = 1; i < N; i++ )
+        printf( " %d", sequence[i] );
+    printf( "\n" );
 }
 
 int main( ) {
-    int N;
+    int N, root;
     cin >> N;
-    for( int i = 0; i < N; i++ ) {
-        char left, right;
-        scanf( "%*c%c %c", &left, &right );
-        tree[i].lchild = CharToInt( left );
-        tree[i].rchild = CharToInt( right );
+    vector<bool> hashTable( N, false );
+    for( int i = 0, l = 0, r = 0; i < N; i++ ) {
+        getchar( );
+        scanf( "%c %c", &l, &r );
+        if( l != 45 ) {
+            tree[i].lchild = l - 48;
+            hashTable[l - 48] = true;
+        } else {
+            tree[i].lchild = -1;
+        }
+        if( r != 45 ) {
+            tree[i].rchild = r - 48;
+            hashTable[r - 48] = true;
+        } else {
+            tree[i].rchild = -1;
+        }
     }
-    int root = FindRoot( N );
+    for( int i = 0; i < N; i++ )
+        if( hashTable[i] == false ) {
+            root = i;
+            break;
+        }
     PostOrder( root );
     BFS( root );
+    Print( N, level );
     InOrder( root );
-    cout << level[0];
-    for( int i = 1; i < N; i++ )
-        cout << " " << level[i];
-    cout << '\n' << in[0];
-    for( int i = 1; i < N; i++ )
-        cout << " " << in[i];
+    Print( N, in );
     return 0;
 }

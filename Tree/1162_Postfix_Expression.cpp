b6c6a76f1c93789c8dca6b2b67f49e9ba93b5526
Jianyang Zhang <82556150+LT-IssacF@users.noreply.github.com>
2022-03-03 16:07:43 +0800

    Update 1162_Postfix_Expression.cpp

--- Tree/1162_Postfix_Expression.cpp
+++ Tree/1162_Postfix_Expression.cpp
@@ -7,39 +7,33 @@ struct node {
     string data;
     int lchild, rchild;
 };
-vector<node> tree;
-void PostTravel( const int &now ) { // 每棵子树都有三种情况，需要注意括号的位置
-    if( tree[now].lchild == -1 && tree[now].rchild == -1 ) { // 第一种叶结点，直接括号包起来输出
-        cout << "(" + tree[now].data;
-    } else if( tree[now].lchild == -1 && tree[now].rchild != -1 ) { // 第二种左孩子空右孩子不空，先把左括号和当前data输出了，再递归处理右孩子，最后再输出自己的右括号
-        cout << "(" + tree[now].data;
-        PostTravel( tree[now].rchild );
-    } else { // tree[now].lchild != -1 && tree[now].rchild != -1
-        cout << "("; // 最后一种先把左括号输出了，再按后序遍历递归处理，最后再输出自己的右括号
-        PostTravel( tree[now].lchild );
-        PostTravel( tree[now].rchild );
-        cout << tree[now].data;
+string PostOrder( const int &now, const vector<node> &tree ) {
+    if( tree[now].lchild < 0 ) {
+        if( tree[now].rchild < 0 ) { // 叶结点
+            return '(' + tree[now].data + ')';
+        } else { // 没有左子树，正常往右子树上递归
+            return '(' + tree[now].data + PostOrder( tree[now].rchild, tree ) + ')';
+        }
+    } else { // 左右孩子都在，左右根后续递归
+        return '(' + PostOrder( tree[now].lchild, tree ) + PostOrder( tree[now].rchild, tree ) + tree[now].data + ')';
     }
-    cout << ")";
 }
 
 int main( ) {
-    int N, root;
+    int N, root = 1;
     cin >> N;
-    tree.resize( N + 1 );
-    vector<int> hashTable( N + 1, false );
+    vector<node> tree( N + 1 );
+    vector<bool> hashTable( N + 1, false );
     for( int i = 1; i <= N; i++ ) {
-        cin >> tree[i].data >> tree[i].lchild >> tree[i].rchild;
-        if( tree[i].lchild != -1 )
+        cin >> tree[i].data;
+        scanf( "%d %d", &tree[i].lchild, &tree[i].rchild );
+        if( tree[i].lchild >= 0 )
             hashTable[tree[i].lchild] = true;
-        if( tree[i].rchild != -1 )
+        if( tree[i].rchild >= 0 )
             hashTable[tree[i].rchild] = true;
     }
-    for( int i = 1; i <= N; i++ )
-        if( hashTable[i] == false ) {
-            root = i;
-            break;
-        }
-    PostTravel( root );
+    while( root <= N && hashTable[root] == true )
+        root++;
+    printf( "%s", PostOrder( root, tree ).c_str( ) );
     return 0;
 }

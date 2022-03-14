33ed5a26daee6c957decf83806efec469d388198
Jianyang Zhang <82556150+LT-IssacF@users.noreply.github.com>
2022-03-14 11:28:49 +0800

    Create 1120_Friend_Numbers.cpp

---
+++ Simple-Simulate/1120_Friend_Numbers.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main( ) {
+    int N;
+    cin >> N;
+    set<int> ids;
+    string num;
+    for( int i = 0, sum = 0; i < N; i++, sum = 0 ) {
+        cin >> num;
+        for( const char &c : num )
+            sum += c - '0';
+        ids.insert( sum );
+    }
+    cout << ids.size( ) << endl;
+    for( auto it = ids.begin( ); it != ids.end( ); it++ )
+        if( it == ids.begin( ) ) {
+            cout << *it;
+        } else {
+            cout << " " << *it;
+        }
+    return 0;
+}

67d54e36002384df5e203a8fde0264f62e2e3e3f
Jianyang Zhang <82556150+LT-IssacF@users.noreply.github.com>
2022-03-18 21:27:00 +0800

    Create 1162_Postfix_Expression.cpp

--- Tree/1162_Postfix_Expression.cpp
+++ Tree/1162_Postfix_Expression.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cstdio>
 #include <string>
 #include <vector>
 using namespace std;
@@ -7,15 +6,15 @@ struct node {
     string data;
     int lchild, rchild;
 };
-string PostOrder( const int &now, const vector<node> &tree ) {
+string PostTravals( const int &now, const vector<node> &tree ) {
     if( tree[now].lchild < 0 ) {
         if( tree[now].rchild < 0 ) { // 叶结点
-            return '(' + tree[now].data + ')';
-        } else { // 没有左子树，正常往右子树上递归
-            return '(' + tree[now].data + PostOrder( tree[now].rchild, tree ) + ')';
+            return "(" + tree[now].data + ")";
+        } else { // 没有左子树，得先根再往右子树上递归
+            return "(" + tree[now].data + PostTravals( tree[now].rchild, tree ) + ")";
         }
     } else { // 左右孩子都在，左右根后续递归
-        return '(' + PostOrder( tree[now].lchild, tree ) + PostOrder( tree[now].rchild, tree ) + tree[now].data + ')';
+        return "(" + PostTravals( tree[now].lchild, tree ) + PostTravals( tree[now].rchild, tree ) + tree[now].data +  ")";
     }
 }
 
@@ -25,15 +24,14 @@ int main( ) {
     vector<node> tree( N + 1 );
     vector<bool> hashTable( N + 1, false );
     for( int i = 1; i <= N; i++ ) {
-        cin >> tree[i].data;
-        scanf( "%d %d", &tree[i].lchild, &tree[i].rchild );
-        if( tree[i].lchild >= 0 )
+        cin >> tree[i].data >> tree[i].lchild >> tree[i].rchild;
+        if( tree[i].lchild > 0 )
             hashTable[tree[i].lchild] = true;
-        if( tree[i].rchild >= 0 )
+        if( tree[i].rchild > 0 )
             hashTable[tree[i].rchild] = true;
     }
     while( root <= N && hashTable[root] == true )
         root++;
-    printf( "%s", PostOrder( root, tree ).c_str( ) );
+    printf( "%s", PostTravals( root, tree ).c_str( ) );
     return 0;
 }

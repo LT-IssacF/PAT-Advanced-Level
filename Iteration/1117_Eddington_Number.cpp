cb49c03b3dacbb88e4b9b9be8a77b6ba767e871d
Jianyang Zhang <82556150+LT-IssacF@users.noreply.github.com>
2022-04-08 09:22:09 +0800

    Create 1117_Eddington_Number.cpp

---
+++ Iteration/1117_Eddington_Number.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main( ) {
+    int N, ans = 0;
+    cin >> N;
+    vector<int> arr( N );
+    for( int i = 0; i < N; i++ )
+        scanf( "%d", &arr[i] );
+    sort( arr.begin( ), arr.end( ), [] ( const int &a, const int &b ) { return a > b; } );
+    // 骑车距离大于等于arr[i]的天数有i + 1天，所以骑车距离大于arr[i] - 1的有i + 1天
+    while( ans < N && arr[ans] > ans + 1 )
+        ans++;
+    cout << ans;
+    return 0;
+} // 10 9 8 8 7 7 7 6 6 3

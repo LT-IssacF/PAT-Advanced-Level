4acf06846c7088eb94a3774a0b673e8e420b8f74
Jianyang Zhang <82556150+LT-IssacF@users.noreply.github.com>
2022-04-13 12:41:27 +0800

    Update 1155_Heap_Paths.cpp

--- Tree/1155_Heap_Paths.cpp
+++ Tree/1155_Heap_Paths.cpp
@@ -2,40 +2,35 @@
 #include <cstdio>
 #include <vector>
 using namespace std;
-vector<int> heap, ans;
-void DFS( const int &start, const int &root, const int &N ) {
-    if( start > N )
+void DFS( const int &N, const int &now, vector<int> &heap, vector<int> &path ) {
+    if( now > N ) { // 越界
         return;
-    ans.push_back( heap[start] );
-    if( start != root ) {
-        vector<int>::iterator it = ans.end( ) - 1; // it指向刚入栈的新元素
-        if( heap[0] == 1 ) { // 大顶堆的情况下
-            if( *( it - 1 ) < *it ) // 如果子结点的值大于父结点的值那就不是堆
-                heap[0] = 0;
-        } else if( heap[0] == 2 ) { // 小顶堆亦相同
-            if( *( it - 1 ) > *it )
-                heap[0] = 0;
+    } else {
+        path.push_back( heap[now] );
+        if( heap[0] > 0 && path.size() >= 2 ) { // 待路径长度大于等于2且之前依旧符合堆的性质再判断
+            int last = path.back( ), secondLast = *( path.end( ) - 2 );
+            heap[0] = heap[0] == 1 ? ( secondLast >= last ? 1 : 0 ) : ( secondLast < last ? 2 : 0 );
         }
-    }
-    if( start > N / 2 ) { // 叶结点
-        printf( "%d", ans[0] );
-        for( int i = 1; i < ans.size( ); i++ )
-            printf( " %d", ans[i] );
-        printf( "\n" );
-    }
-    DFS( 2 * start + 1, root, N );
-    DFS( 2 * start, root, N );
-    ans.pop_back( ); // 出栈
-} // 右 左 根
+        if( now > N / 2 ) { // 叶结点就输出整条路径
+            printf( "%d", path[0] );
+            for( int i = 1; i < path.size(); i++ )
+                printf( " %d", path[i] );
+            printf( "\n" );
+        }
+        DFS( N, now * 2 + 1, heap, path );
+        DFS( N, now * 2, heap, path );
+        path.pop_back( ); // 出栈
+    } // 根 右 左
+}
 
 int main( ) {
     int N;
     cin >> N;
-    heap.resize( N + 1 );
+    vector<int> path, heap( N + 1, 0 );
     for( int i = 1; i <= N; i++ )
         scanf( "%d", &heap[i] );
-    heap[0] = ( heap[1] >= heap[2] ? 1 : 2 );
-    DFS( 1, 1, N );
-    cout << ( heap[0] == 0 ? "Not Heap" : ( heap[0] == 1 ) ? "Max Heap" : "Min Heap" );
+    heap[0] = heap[1] >= heap[2] ? 1 : 2; // 先根据根结点和它孩子的大小关系判断是否是堆
+    DFS( N, 1, heap, path );
+    cout << ( heap[0] > 0 ? ( heap[0] == 1 ? "Max Heap" : "Min Heap" ) : "Not Heap" );
     return 0;
 }

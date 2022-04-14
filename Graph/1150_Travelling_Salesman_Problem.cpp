9e15d03b2392fc65d908fed193754dc92313b386
Jianyang Zhang <82556150+LT-IssacF@users.noreply.github.com>
2022-04-14 19:34:14 +0800

    Update 1150_Travelling_Salesman_Problem.cpp

--- Graph/1150_Travelling_Salesman_Problem.cpp
+++ Graph/1150_Travelling_Salesman_Problem.cpp
@@ -1,57 +1,47 @@
 #include <iostream>
 #include <cstdio>
-#include <vector>
-#include <set>
-#define MAX 0x3fffffff // 不可达
+#include <unordered_set>
 using namespace std;
-int G[202][202] = { 0 }; // 图
-vector<bool> hashTable; // 标记城市是否已访问过
-void Check( const int &N, const int &now, int &MIN, int &num ) {
-    int n, sum = 0;
-    bool flag = true; // 标记路径是否能走通
-    scanf( "%d", &n ); 
-    vector<int> v( n );
-    set<int> s; // set的size可以检查是否所有城市都访问过
-    for( int i = 0; i < n; i++ ) {
-        scanf( "%d", &v[i] );
-        s.insert( v[i] );
-        if( i != 0 ) {
-            sum += G[v[i - 1]][v[i]];
-            if( G[v[i - 1]][v[i]] == 0 )
-                flag = false;
-        }
-    }
-    if( !flag ) {
-        printf( "NA (Not a TS cycle)\n" );
-    } else if( v[0] != v[n - 1] || s.size( ) != N ) {
-        printf( "%d (Not a TS cycle)\n", sum );
-    } else {
-        if( MIN > sum ) {
-            MIN = sum;
-            num = now;
-        }
-        if( n == N + 1 ) {
-            printf( "%d (TS simple cycle)\n", sum );
-        } else {
-            printf( "%d (TS cycle)\n", sum );
-        }
-    }
-}
+int G[201][201] = { 0 };
 
 int main( ) {
-    int N, M, K, MIN = MAX, num = 1;
+    int N, M, K, shortestIndex = 0, shortestDis = 0x7fffffff;
     cin >> N >> M;
-    hashTable.resize( N + 1 );
-    for( int i = 0, v1 = 0, v2 = 0, d = 0; i < M; i++ ) {
-        scanf( "%d %d %d", &v1, &v2, &d );
-        G[v1][v2] = G[v2][v1] = d;
+    for( int i = 0, v = 0, v2 = 0, dis = 0; i < M; i++ ) {
+        scanf( "%d %d %d", &v, &v2, &dis );
+        G[v][v2] = G[v2][v] = dis;
     }
     cin >> K;
-    for( int i = 0, temp = 0; i < K; i++ ) {
-        fill( hashTable.begin( ), hashTable.end( ), false );
-        printf( "Path %d: ", i + 1 );
-        Check( N, i + 1, MIN, num );
+    for( int i = 1, n = 0, v = 0, v2 = 0; i <= K; i++ ) {
+        scanf( "%d %d", &n, &v );
+        unordered_set<int> cities; // 判断是否访问了每个城市
+        int totalDis = 0, start = v; // 记下起点城市
+        for( int j = 1; j < n; j++ ) {
+            cities.insert( v );
+            scanf( "%d", &v2 );
+            if( G[v][v2] == 0 ) { // 走不通
+                totalDis = -1;
+            } else if( totalDis >= 0 ) {
+                totalDis += G[v][v2];
+            }
+            v = v2;
+        }
+        if( totalDis < 0 ) {
+            printf( "Path %d: NA (Not a TS cycle)\n", i );
+        } else if( cities.size( ) == N && start == v2 ) { // 每个城市都访问了且回到了起点，无论是否走没走弯路
+            if( totalDis < shortestDis ) {
+                shortestDis = totalDis;
+                shortestIndex = i;
+            }
+            if( n == N + 1 ) { // 没走弯路
+                printf( "Path %d: %d (TS simple cycle)\n", i, totalDis );
+            } else { // 走弯路
+                printf( "Path %d: %d (TS cycle)\n", i, totalDis );
+            }
+        } else { // 没访问所有城市或者没回到起点
+            printf( "Path %d: %d (Not a TS cycle)\n", i, totalDis );
+        }
     }
-    printf( "Shortest Dist(%d) = %d", num, MIN );
+    printf( "Shortest Dist(%d) = %d", shortestIndex, shortestDis );
     return 0;
 }

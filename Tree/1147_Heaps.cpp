c74701683525e995a1f507b340433a1835411b05
Jianyang Zhang <82556150+LT-IssacF@users.noreply.github.com>
2022-04-17 21:59:42 +0800

    Update 1147_Heaps.cpp

--- Tree/1147_Heaps.cpp
+++ Tree/1147_Heaps.cpp
@@ -1,49 +1,52 @@
 #include <iostream>
 #include <cstdio>
-#include <vector>
 using namespace std;
 struct node {
-    int data, lchild, rchild;
-};
-vector<node> tree;
-void PostOrder( const int &root, const int &start ) {
-    if( start == 0 )
+    int data = 0, lchild = 0, rchild = 0;
+} tree[0x3ff];
+void PostTravals( const int &N, const int &now, bool &flag ) {
+    if( now == 0 ) {
         return;
-    PostOrder( root, tree[start].lchild );
-    PostOrder( root, tree[start].rchild );
-    cout << tree[start].data << ( start == root ? "\n" : " " );
+    } else if( now <= N ) {
+        PostTravals( N, tree[now].lchild, flag );
+        PostTravals( N, tree[now].rchild, flag );
+        if( flag == false ) {
+            printf( "%d", tree[now].data );
+            flag = true;
+        } else {
+            printf( " %d", tree[now].data );
+        }
+    }
 }
 
 int main( ) {
     int M, N;
     cin >> M >> N;
-    vector<int> arr( N + 1 );
-    tree.resize( N + 1 );
-    for( int i = 0, j = 1, key = 0; i < M; i++ ) {
-        for( j = 1; j <= N; j++ ) {
-            scanf( "%d", &arr[j] );
-            tree[j].data = arr[j];
-        }
-        arr[0] = arr[1] >= arr[2] ? 1 : 2; // arr[0] = 1大顶堆，2小顶堆
-        for( j = 1, key = 0; j <= N / 2; j++ ) { // arr[N / 2]是最后一个父结点，堆的性质
-            key = arr[2 * j]; // 先令key为父结点的左孩子的值
-            if( arr[0] == 1 ) { // 大顶堆
-                if( 2 * j + 1 <= N && key < arr[2 * j + 1] ) // 如果右孩子存在且右孩子的值更大
-                    key = arr[2 * j + 1];
-                if( arr[j] < key ) // 一旦出现孩子的值比父结点的还大就不是大顶堆
-                    arr[0] = 0;
-            } else { // 小顶堆亦同理
-                if( 2 * j + 1 <= N && key > arr[2 * j + 1] )
-                    key = arr[2 * j + 1];
-                if( arr[j] >= key )
-                    arr[0] = 0;
+    for( int i = 0; i < M; i++ ) {
+        for( int j = 1; j <= N; j++ ) {
+            scanf( "%d", &tree[j].data );
+            if( j <= N / 2 ) { // 为非叶结点设置左右指针
+                tree[j].lchild = 2 * j, tree[j].rchild = 2 * j + 1;
             }
-            tree[j].lchild = 2 * j; // 顺便把树建了
-            if( 2 * j + 1 <= N )
-                tree[j].rchild = 2 * j + 1;
+            if( j == 2 ) { // 用第二个结点和根结点判断堆的性质
+                tree[0].data = tree[1].data >= tree[2].data ? 1 : 2; // tree[0].data储存堆最初的性质
+            }
+            if( tree[0].data > 0 && j > 2 ) { // 之前的结点依旧满足堆的性质再判断
+                if( tree[0].data == 1 ) { // 大顶堆
+                    if( tree[j / 2].data < tree[j].data ) { // 父结点的值小于孩子结点的值就不是堆
+                        tree[0].data = 0;
+                    }
+                } else { // 小顶堆
+                    if( tree[j / 2].data >= tree[j].data ) {// 父结点的值大于等于孩子结点的值就不是堆
+                        tree[0].data = 0;
+                    }
+                }
+            }
         }
-        cout << ( arr[0] == 0 ? "Not Heap" : ( arr[0] == 1 ? "Max Heap" : "Min Heap" ) ) << endl;
-        PostOrder( 1, 1 );
+        printf( "%s\n", tree[0].data > 0 ? ( tree[0].data == 1 ? "Max Heap" : "Min Heap" ) : "Not Heap" );
+        bool flag = false;
+        PostTravals( N, 1, flag );
+        printf( "\n" );
     }
     return 0;
 }

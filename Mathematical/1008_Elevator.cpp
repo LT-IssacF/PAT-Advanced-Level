3769f8ef883fd52dbb55b015dd7a2de0f4b4249f
Jianyang Zhang <82556150+LT-IssacF@users.noreply.github.com>
2022-04-18 19:55:30 +0800

    Create 1008_Elevator.cpp

---
+++ Mathematical/1008_Elevator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+// 签到题
+int main( ) {
+    int N, now_floor, pre_floor = 0, ascend = 6, descend = 4, stay = 5;
+    cin >> N >> now_floor;
+    int ans = ( now_floor - pre_floor ) * ascend + stay;
+    for( int i = 1; i < N; i++ ) {
+        pre_floor = now_floor;
+        scanf( "%d", &now_floor );
+        ans += ( now_floor > pre_floor ? ( now_floor - pre_floor ) * ascend : ( pre_floor - now_floor ) * descend ) + stay;
+    }
+    cout << ans;
+}

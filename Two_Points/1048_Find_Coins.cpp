188816f13bd664e5312bbaa07b185c373196bfc4
Jianyang Zhang <82556150+LT-IssacF@users.noreply.github.com>
2022-06-17 19:31:42 +0800

    Create 1048_Find_Coins.cpp

---
+++ Two_Points/1048_Find_Coins.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main( ) {
+    int N, M;
+    scanf( "%d %d", &N, &M );
+    vector<int> coins;
+    for( int i = 0, temp = 0; i < N; i++ ) {
+        scanf( "%d", &temp );
+        coins.push_back( temp );
+    }
+    sort( coins.begin( ), coins.end( ) );
+    int left = 0, right = N - 1;
+    for( ; left <= right; ) {
+        if( coins[left] + coins[right] > M )
+            right--;
+        else if( coins[left] + coins[right] < M )
+            left++;
+        else
+            break;
+    }
+    if( left >= right ) // 注意条件
+        cout << "No Solution";
+    else
+        cout << coins[left] << " " << coins[right];
+    return 0;
+}

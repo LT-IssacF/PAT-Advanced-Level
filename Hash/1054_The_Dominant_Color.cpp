b0f2e2ca74f7dfc69a6b857ff16b87df6b2d71fd
Jianyang Zhang <82556150+LT-IssacF@users.noreply.github.com>
2022-07-21 21:29:33 +0800

    Create 1054_The_Dominant_Color.cpp

---
+++ Hash/1054_The_Dominant_Color.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main( ) {
+    int N, M, min = 0, ans;
+    cin >> N >> M;
+    map<int, int> m;
+    for( int i = 0, color = 0; i < M; i++ ) {
+        for( int j = 0; j < N; j++ ) {
+            scanf( "%d", &color );
+            m[color]++;
+        }
+    }
+    for( map<int, int>::iterator it = m.begin( ); it != m.end( ); it++ ) {
+        if( it->second > min ) {
+            ans = it->first;
+            min = it->second;
+        }
+    }
+    cout << ans;
+    return 0;
+}
